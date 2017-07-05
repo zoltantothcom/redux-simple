518b9ea171069754a6d6907e5305db11ffffad99
Zoltan Toth <contact@zoltantoth.com>
2017-07-05 10:45:16 -0400

    creating an action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log(book.title)
+  // selectBook is an ActionCreator
+  // it needs to return an action - object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
