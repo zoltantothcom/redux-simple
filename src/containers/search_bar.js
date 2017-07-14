f146553fd47265c41fd52b5a51138f3c47c26274
Zoltan Toth <contact@zoltantoth.com>
2017-07-14 06:38:19 -0400

    form onSubmit event handled

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,17 +5,22 @@ export default class SearchBar extends Component {
     super(props);
 
     this.state = { term: '' };
-    
+
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
