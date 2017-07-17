f544f37053d3895d0cee5c419cf5696914e39979
Zoltan Toth <contact@zoltantoth.com>
2017-07-17 06:42:40 -0400

    added Axios and constructed a GET request with it

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '5fd30f2890a7a490d82cf69784183f59';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ca`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
