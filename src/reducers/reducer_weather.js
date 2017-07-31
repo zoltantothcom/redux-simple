d0a9626f619192c1e09556fc184de8fbb2705c45
Zoltan Toth <contact@zoltantoth.com>
2017-07-31 06:30:42 -0400

    wired aup the weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FEATCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FEATCH_WEATHER:
+      // return state.concat([action.payload.data]);
+      return [action.payload.data, ...state];
+  }
+
   return state;
 }
